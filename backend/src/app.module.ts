bebb8f10df8d96ee412fb8c6d6f796769b23efcf
hummingengineer <hummingengineer@gmail.com>
2023-02-10 23:52:25 +0900

    feat: Add typePaths

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      include: [],
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+      debug: false,
+      playground: false,
     }),
   ],
 })
