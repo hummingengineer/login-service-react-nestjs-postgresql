52e4834ab4acc023e935ed87eb8c596c7e1a9a2d
hummingengineer <hummingengineer@gmail.com>
2023-03-15 23:55:44 +0900

    feat: Add IsNotEmpty decorator to email

--- backend/src/users/dto/create-user.input.ts
+++ backend/src/users/dto/create-user.input.ts
@@ -1,7 +1,9 @@
 import { Field } from '@nestjs/graphql';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateUserInput {
   @Field()
+  @IsNotEmpty()
   email: string;
 
   @Field()
