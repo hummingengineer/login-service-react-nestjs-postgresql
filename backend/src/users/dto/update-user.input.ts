8cb60acf4f3d113abfcd57f32bdc28307be90b77
hummingengineer <hummingengineer@gmail.com>
2023-03-21 22:31:55 +0900

    feat: Add InputType decorator

--- backend/src/users/dto/update-user.input.ts
+++ backend/src/users/dto/update-user.input.ts
@@ -1,6 +1,8 @@
+import { InputType } from '@nestjs/graphql';
 import { CreateUserInput } from './create-user.input';
 import { PartialType, OmitType } from '@nestjs/mapped-types';
 
+@InputType()
 export class UpdateUserInput extends PartialType(
   OmitType(CreateUserInput, ['email'] as const),
 ) {
