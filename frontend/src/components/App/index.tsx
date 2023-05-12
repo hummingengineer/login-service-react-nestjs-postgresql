9620696ddf194a0f2a7535435e6fd1cbb062084d
hummingengineer <hummingengineer@gmail.com>
2023-05-12 14:18:48 +0900

    feat: Update query

--- frontend/src/components/App/index.tsx
+++ frontend/src/components/App/index.tsx
@@ -6,7 +6,7 @@ export default function App() {
   const handleFormSubmit = useCallback(
     async (event: React.FormEvent<HTMLFormElement>) => {
       const query = `{
-        remove(id: 40)
+        findOne(id: 40)
       }`;
       const res = await fetchGraphQL(query);
       console.log(res);
